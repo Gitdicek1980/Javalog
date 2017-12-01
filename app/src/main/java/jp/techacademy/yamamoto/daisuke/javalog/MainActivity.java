91c7bbca3a8cd9dc9fc3c1c5ed24ee9201605652
d_yamamoto <nice_day81@yahoo.co.jp>
2017-12-01 15:37:22 +0900

    修正してみましたので、確認をお願いいたします。

--- app/src/main/java/jp/techacademy/yamamoto/daisuke/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/yamamoto/daisuke/javalog/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         HUMAN human = new HUMAN("テックアカデミー", 1);     // 名前をテックアカデミー、年齢1歳で、Humanのインスタンスを作る
 
-        HUMAN.say();
+        human.say();
         Log.d("javatest", "私の名前は" + human.name + "です。");
         Log.d("javatest", "私の年齢は" + human.age + "歳です。");
 
